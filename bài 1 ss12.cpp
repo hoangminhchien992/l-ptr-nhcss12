94cf0c6290d636d76970b1e0fe98448e0fb97e3a
hoangminhchien992 <hoangminhchien722@gmail.com>
2024-12-10 08:21:07 +0700

    btvn c ss12

---
+++ bài 1 ss12.cpp
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main(){
+	float number1 , number2 ; 
+	float total ;
+	printf("Moi ban nhap so thu nhat : " );
+	scanf("%f",&number1);
+	printf("Moi ban nhap so thu hai : " );
+	scanf("%f",&number2);
+	total = tinhTong(number1, #include<stdio.h>
+ number2);
+	printf("Tong hai so la : %.1f",total);	
+	return 0 ; 
+ } 
+ float tinhTong(float num1 , float num2 )
+ {
+	float sum = num1 + num2 ;
+	return sum ; 
+ }
